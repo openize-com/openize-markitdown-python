3d249052eb9ac785e8724ed3b696db81a8dc8e2e
fileformat-slides-gists <muhammad.umar@groupdocs.com>
2025-03-18 14:07:12 +0500

    For v 25.3.12

--- packages/markitdown/src/openize/markitdown/__init__.py
+++ packages/markitdown/src/openize/markitdown/__init__.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.dirname(__file__))
 
-__version__ = "25.3.10"
+__version__ = "25.3.12"
 
 from .processor import DocumentProcessor
 from .converters import WordConverter, PDFConverter, ExcelConverter, PowerPointConverter
